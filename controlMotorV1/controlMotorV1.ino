f32ab1328b71a18921e72022caeb3c909a42cd60
Gaelongo <113254167+Gaelongo@users.noreply.github.com>
2023-03-12 20:01:39 -0700

    V3
    Se agregó la conexión con el control.

--- controlMotorV1/controlMotorV1.ino
+++ controlMotorV1/controlMotorV1.ino
@@ -1,3 +1,6 @@
+#include <Ps3Controller.h>
+
+// Pines de los cables de los motores
 const int M1L = 5;//Motor izquierdo hacia adelante
 const int M1R = 18;//Motor izquierdo hacia atras
 const int M2L = 22;//Motor derecho hacia adelante
@@ -6,18 +9,28 @@ const int ARML = 16;//Arma
 const int ARMR = 4;//arma
 
 // Propiedades PWM
-
 const int frecuencia = 5000;
 const int canalM1L = 0;
 const int canalM1R = 1;
 const int canalM2L = 2;
 const int canalM2R = 3;
 const int canalARML = 4;
-const int canal ARMR = 5;
+const int canalARMR = 5;
 const int resolucion = 8;
 
+// Variable para la batería del control
+int bateria;
+
+// Mensaje al conectarse el control
+void onConnect(){
+  Serial.println("Control conectado.");
+}
 
 void setup() {
+
+  Serial.begin(115200);
+
+  // Configuración de los pines de los motores como salidas
   pinMode(M1L, OUTPUT);
   pinMode(M1R, OUTPUT);
   pinMode(M2L, OUTPUT);
@@ -25,27 +38,43 @@ void setup() {
   pinMode(ARML, OUTPUT);
   pinMode(ARMR, OUTPUT);
 
-  // configuración de la funcionalidad pwm
-
+  // configuración de la funcionalidad PWM
   ledcSetup(canalM1L, frecuencia, resolucion);
   ledcSetup(canalM1R, frecuencia, resolucion);
   ledcSetup(canalM2L, frecuencia, resolucion);
   ledcSetup(canalM2R, frecuencia, resolucion);
+  ledcSetup(canalARML, frecuencia, resolucion);
+  ledcSetup(canalARMR, frecuencia, resolucion);
 
+  // Asignación de canales PWM a cada cable de motor
   ledcAttachPin(M1L, canalM1L);
   ledcAttachPin(M1R, canalM1R);
   ledcAttachPin(M2L, canalM2L);
   ledcAttachPin(M2R, canalM2R);
-  ledcAttachPin(ARML, canal);
-  ledcAttachPin(ARMR, canal);
+  ledcAttachPin(ARML, canalARML);
+  ledcAttachPin(ARMR, canalARMR);
+
+  // Configuración e inicio del control 
+  Ps3.attachOnConnect(onConnect);
+  Ps3.begin("b8:27:eb:52:7e:31");
+  Serial.println("Listo para conectar.");
+  
 }
 
 void loop() {
+
+  if(!Ps3.isConnected()){
+    Serial.println("Control desconectado.");
+    return;
+  }
   int leftJoyIn = Ps3.data.analog.stick.ly;
 
-  int rightJoyIn = PS3.data.analog.sticky.rx;
+  int rightJoyIn = Ps3.data.analog.stick.rx;
 
-  if(leftJoyIn<-10 && rightJoyIn==0){
+  if(leftJoyIn<-10 && rightJoyIn==-1){
+
+    Serial.println("Avanzando");
+    
     //Mover hacia enfrente (ambos motores giran hacia adelante)
 
     int motorPowerDR = map((leftJoyIn*(-1)), 0, 128, 0, 255);
@@ -58,7 +87,10 @@ void loop() {
     ledcWrite(canalM1L, motorPowerDR);
     ledcWrite(canalM2L, motorPowerDR); 
 
-  }else if(leftJoyIn>10 && rightJoyIn==0){
+  }else if(leftJoyIn>10 && rightJoyIn==-1){
+
+    Serial.println("Retrocediendo");
+    
     //Mover hacia atras (ambos motores giran hacia atras)
 
     int motorPowerDR = map(leftJoyIn, 0, 128, 0, 255);
@@ -71,7 +103,10 @@ void loop() {
     ledcWrite(canalM1R, motorPowerDR);
     ledcWrite(canalM2R, motorPowerDR);
 
-  }else if(leftJoyIn== 0 && rightJoyIn>10){
+  }else if(leftJoyIn==-1 && rightJoyIn>10){
+
+    Serial.println("Girando a la derecha");
+
     //Girar hacia la derecha(Motor 1 hacia enfrente y motor 2 hacia atras)
 
     int motorPowerLR = map(rightJoyIn, 0, 128, 0, 255);
@@ -84,7 +119,10 @@ void loop() {
     ledcWrite(canalM1L, motorPowerLR);//motor 1 hacia enfrente potencia del joystick
     ledcWrite(canalM2R, motorPowerLR);//motor 2 hacia atras potencia del joystick
 
-  }else if(leftJoyIn==0 && rightJoyIn<-10){
+  }else if(leftJoyIn==-1 && rightJoyIn<-10){
+
+    Serial.println("Girando a la izquierda");
+    
     //Girar hacia la izquierda (motor 1 hacia atras y motor 2 hacia enfrente)
 
     int motorPowerLR = map((rightJoyIn*(-1)), 0,128, 0, 255);
@@ -98,6 +136,9 @@ void loop() {
     ledcWrite(canalM2L, motorPowerLR);
 
   }else if(leftJoyIn<-10 && rightJoyIn<-10){
+
+    Serial.println("Avanzando y girando hacia la izquierda");
+
     //Avanzar + Girar hacia la izquierda (reducir considerablemente la potencia del motor izquierdo)
 
     int motorPowerDR = map((leftJoyIn*(-1)), 0, 128, 0, 255);
@@ -111,6 +152,9 @@ void loop() {
     ledcWrite(canalM2L, motorPowerDR);//Esta continua con la potencia del joystic
 
   }else if(leftJoyIn<-10 && rightJoyIn>10){
+
+    Serial.println("Avanzando y girando hacia la derecha");
+    
     //Avanzar + Girar hacia la derecha (reducir considerablemente la potencia del motor derecho )
 
     int motorPowerDR = map((leftJoyIn*(-1)), 0, 128, 0, 255);
@@ -124,6 +168,9 @@ void loop() {
     ledcWrite(canalM2L, (motorPowerDR/6));//se Reduce la potencia a una sexta parte
 
   }else if(leftJoyIn>10 && rightJoyIn<-10){
+
+    Serial.println("Retrocediendo y girando a la izquierda");
+
     //Retroceder + Girar hacia la izquierda (reducir considerablemente la potencia del motor izquierdo)
 
     int motorPowerDR = map(leftJoyIn, 0, 128, 0, 255);
@@ -137,6 +184,9 @@ void loop() {
     ledcWrite(canalM2R, motorPowerDR);//Esta continua con la potencia del joystic
 
   }else if(leftJoyIn>10 && rightJoyIn>10){
+
+    Serial.println("Retrocediendo y girando a la derecha");
+
     //retoceder + Girar hacia la derecha (reducir considerablemente la potencia del motor derecho)
 
     int motorPowerDR = map(leftJoyIn, 0, 128, 0, 255);
@@ -148,6 +198,17 @@ void loop() {
     //Estos son los que si funcionan
     ledcWrite(canalM1R, motorPowerDR);//Continua la misma potencia
     ledcWrite(canalM2R, (motorPowerDR/6));//Se reduce la potencia a /6
+
+  }else if(leftJoyIn == -1 && rightJoyIn == -1){
+
+    Serial.println("Motores en 0");
+
+      // Todos los canales de los motores de movimiento en 0
+    ledcWrite(canalM1L, 0);
+    ledcWrite(canalM1R, 0);
+    ledcWrite(canalM2L, 0);
+    ledcWrite(canalM2R, 0);
+
   }
 
 }
